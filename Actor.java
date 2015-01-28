1b9c1a5f7baf879fe7cd1173867108da161658be
Jamie <rangedenemy@gmail.com>
2015-01-28 12:26:38 -0500

    added actor super class

---
+++ Actor.java
@@ -0,0 +1,25 @@
+
+public class Actor {
+
+	private int xPos;
+	private int yPos;
+	
+	public Actor(int x, int y){
+		xPos=x;
+		yPos=y;
+	}
+	
+	public int getX(){
+		return xPos;
+	}
+	
+	public int getY(){
+		return yPos;
+	}
+	
+	public void move(){
+		xPos=xPos;
+		yPos=yPos;
+	}
+	
+}
