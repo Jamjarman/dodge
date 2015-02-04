132049226047eef186371412265b56745314aa64
BlueKeleher <BlueKeleher@users.noreply.github.com>
2015-02-03 21:22:28 -0500

    Redrew boundaries with min/maxes

--- DodgeCanvas.java
+++ DodgeCanvas.java
@@ -27,11 +27,10 @@ public class DodgeCanvas extends Canvas implements Runnable {
 	private boolean dead=false;
 	
 	// display fields
-	private int screenX;
-	private int screenY;
-	private int playX;
-	private int playY;
+	private int screenX, screenY;			// actual screen dimensions
+	private int playX, playY;				// margins for playable screen
 	private int[] colorOffsets;
+	private int minX, minY, maxX, maxY;		// minimum and maximum boundaries for actors
 	
 	// final fields
 	private final int LEFT_MARGIN = 180;	// dimensions of bottom margin
@@ -42,8 +41,11 @@ public class DodgeCanvas extends Canvas implements Runnable {
 		//System.out.println("initializing canvas");
 		screenX = frameWidth;
 		screenY = frameHeight;
-		playX = (int)((double)(screenX-LEFT_MARGIN)*initX/2.0);
-		playY = (int)((double)(screenX-LEFT_MARGIN)*initY/2.0);
+		playX = (int)((double)(screenX-LEFT_MARGIN)*(1-initX)/2.0);
+		playY = (int)((double)screenY*(1-initY)/2.0);
+		setMinMaxes();
+		System.out.println("PlayY = "+playY);
+		System.out.println("MaxY = "+maxY);
 		health = initHealth;
 		score = initScore;
 		bubble = colRad;
@@ -75,21 +77,30 @@ public class DodgeCanvas extends Canvas implements Runnable {
 		}); // end of key listener
 		
 		//setEnemyNum(numEnemies);
-		setP(new Player(screenX/2, screenY/2, screenX, screenY, health, score));
+		setP(new Player(screenX/2, screenY/2, screenX, screenY, health, score, minX, maxX, minY, maxY));
 		genEnemies();
 		
 		colorOffsets = chooseColorScheme();
 		repaintReminder = new Thread(this);
 		repaintReminder.start();
+	} // end of constructor
+	
+	private void setMinMaxes() {
+		minX = LEFT_MARGIN+playX;
+		minY = playY;
+		maxX = screenX-playX;
+		maxY = screenY-(playY*2);
 	}
 
+	
+	
 	/**
 	 * generate a set of enemies
 	 */
 	private void genEnemies() {
 		this.eArr=new Enemy[this.numEnemies];
 		for(int i=0; i<numEnemies; i++){
-			this.eArr[i]=new Enemy(Math.random()*screenX, Math.random()*screenY, screenX, screenY);
+			this.eArr[i]=new Enemy(Math.random()*screenX, Math.random()*screenY, screenX, screenY, minX, maxX, minY, maxY);
 			this.eArr[i].getVel().randomize();
 		}
 	}
@@ -181,7 +192,7 @@ public class DodgeCanvas extends Canvas implements Runnable {
 				newArr[i]=eArr[i];
 			}
 			for(int i=this.numEnemies; i<enemyNumNew; i++){
-				newArr[i]=new Enemy(Math.random()*screenX, Math.random()*screenY, screenX, screenY);
+				newArr[i]=new Enemy(Math.random()*screenX, Math.random()*screenY, screenX, screenY, minX, maxX, minY, maxY);
 				newArr[i].getVel().randomize();
 			}
 		}
@@ -203,12 +214,12 @@ public class DodgeCanvas extends Canvas implements Runnable {
 		BasicStroke bs = new BasicStroke(3);
 		g2d.setColor(new Color(colorOffsets[0]+100, colorOffsets[1]+100, colorOffsets[2]+100));
 		g2d.setStroke(bs);
-		g2d.drawRect(LEFT_MARGIN+OTHER_MARGINS-4, OTHER_MARGINS-4, (screenX-2*OTHER_MARGINS-LEFT_MARGIN)+8, (screenY-(6*OTHER_MARGINS))+8);
+		g2d.drawRect(minX, minY, maxX-minX, maxY-minY);
 				
 		// Draws outer border
 		bs = new BasicStroke(6);
 		g2d.setStroke(bs);
-		g2d.drawRect(LEFT_MARGIN+OTHER_MARGINS-10, OTHER_MARGINS-10, (screenX-2*OTHER_MARGINS-LEFT_MARGIN)+21, (screenY-(6*OTHER_MARGINS))+21);
+		g2d.drawRect(minX-10, minY-10, maxX-minX+20, maxY-minY+20);
 		
 		/*
 		 * Displays the colored border for the statistics
